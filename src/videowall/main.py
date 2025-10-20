00073790ac0590806e707d831afaba353952a373
Robin Scher <robin@uberware.net>
2025-10-19 17:10:22 -0700

    Cleanup

--- src/videowall/main.py
+++ src/videowall/main.py
@@ -21,13 +21,6 @@ def main():
     app_font.setPointSize(10)
     app.setFont(app_font)
 
-    # videos = [
-    #     "/Volumes/Movies-4/Godzilla.vs.Kong.2021.1080p.x264.ac3-nibo.mp4",
-    #     "/Volumes/Movies-4/Groundhog.Day.1993.REMASTERED.1080p.BluRay.6CH.ShAaNiG.mp4",
-    #     "/Volumes/Movies-4/Jumanji (1995).1080p-x264-ac3.Nibo.mp4",
-    #     "/Volumes/Movies-4/Jumanji (2017).1080p-x264-ac3.Nibo.mp4",
-    # ]
-
     window = MainWindow()
     window.show()
     logger.info("Running main loop")
